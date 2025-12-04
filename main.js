8b62a87794dbb8fa4608cc9ddcef85008d871fe6
Joeliii <joelboy12704@gmail.com>
2025-12-04 12:52:34 -0500

    Initial Requirements and shell

---
+++ main.js
@@ -0,0 +1,12 @@
+// main.js (starter skeleton)
+
+// TODO: Add OOP classes here in Iteration 1
+// class Card {}
+// class Shoe {}
+// class Hand {}
+// class Player {}
+// class Dealer {}
+// class Game {}
+
+// Temporary placeholder until Iteration 1
+console.log("Blackjack project initialized.");
